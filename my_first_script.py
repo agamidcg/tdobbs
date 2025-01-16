2dcf301c904957c8f6bff6eb5f78b8431f776269
Tim Dobbs <tdobbs@proton.me>
2025-01-16 16:14:01 -0600

    Added additional exercises

--- my_first_script.py
+++ my_first_script.py
@@ -1,14 +1,29 @@
 # print("Hello, world!")
 
+#Exercise 1: Calculation
 
-bread = 2.41
-eggs = 5.28
-milk = 2.56 
+# bread = 2.41
+# eggs = 5.28
+# milk = 2.56 
 
-total_cost = bread + eggs + milk
-print(f"Your total is ${total_cost} Thank you!")
+# total_cost = bread + eggs + milk
+# print(f"Your total is ${total_cost} Thank you!")
 
+# Exercise 2: String Manipulation
 
+# this line takes the user input for the store name
+# store = input("Please enter store name: ")
+# # This prints a welcome message using user input as store name
+# print(f"Welcome to {store}!")
 
+#Exercise 3: Common Errors
 
-
+# milk = "3"
+# bread = 2.5
+# total = milk + bread
+# print("The total cost is: $" + total
+ # removed the quotes from the 3 you can't use strings in Math problems
+milk = 3
+bread = 2.5
+total = milk + bread
+print(f"The total cost is: $",total)
